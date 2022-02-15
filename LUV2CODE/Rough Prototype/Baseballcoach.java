49c9126a024f0f1f568e1390d8545f6aff4cb424
Arpit <arpit27kr@gmail.com>
2022-02-14 22:00:09 -0800

    Add files via upload

---
+++ LUV2CODE/Rough Prototype/Baseballcoach.java
@@ -0,0 +1,13 @@
+package com.luv2code.springdemo;
+
+public class Baseballcoach implements coach {
+	
+	
+
+	public String getDailyworkout() {
+		// TODO Auto-generated method stub
+		return "I have done 30 mins of workout ";
+	}
+
+	
+}

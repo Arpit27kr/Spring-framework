9cd19b035d5eb461dbf74a8deafa16f0a8ddf362
Arpit <arpit27kr@gmail.com>
2022-02-21 12:34:13 -0800

    Add files via upload

---
+++ LUV2CODE/Annotations/@Configuration and @ComponentScan/TrackMe.java
@@ -0,0 +1,19 @@
+package com.luv2code.noxml;
+
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class TrackMe implements name{
+	
+	public String myname()
+	{
+		return "My name is Arpit";
+	}
+	public String yourname()
+	{
+		return "your name is user";
+	}
+	
+
+}

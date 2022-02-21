9ea1357e65032943a405930bf9df4f7c522cd933
Arpit <arpit27kr@gmail.com>
2022-02-21 12:33:43 -0800

    Add files via upload

---
+++ LUV2CODE/Annotations/@Configuration and @ComponentScan/SportConfig.java
@@ -0,0 +1,10 @@
+package com.luv2code.noxml;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.luv2code.noxml")
+public class SportConfig {
+
+}
